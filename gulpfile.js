ebb6b26c59fb47f81ae999cade9385b876fea91c
Toby Thomson <me@tobythomson.co.uk>
2021-11-10 02:56:56 +0000

    Added basic gulp bundling feature (.gitignore changed for testing)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,37 @@
-const { series } = require('gulp');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
+var pipeline = require('readable-stream').pipeline;
+var merge = require('merge-stream');
 
+function cleanup (cb) {
+    return pipeline(
+        gulp.src('bundle/*', {read: false}),
+        clean()
+    );
+
+    cb();
+}
 
 // TODO: Add bundling feature here
 function build (cb) {
+    cleanup(cb);
+
+    var first = pipeline(
+        gulp.src('index.html'),
+        gulp.dest('bundle/')
+    );
+
+    var second = pipeline(
+        gulp.src('js/*'),
+        uglify(),
+        concat('bundle.js'),
+        gulp.dest('bundle')
+    );
+
+    return merge(second);
+
     cb();
 }
 
@@ -13,8 +42,9 @@ function test (cb) {
 
 // TODO: Add server start and firefox open here
 function inspect (cb) {
+    build();
     cb();
 }
 
 exports.inspect = inspect;
-exports.default = series(build, test);
+exports.default = gulp.series(build, test);
