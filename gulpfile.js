6bffdf518ca967f9e1be703aca7b36cf551f8ce6
Toby Thomson <me@tobythomson.co.uk>
2021-11-10 01:51:51 +0000

    Written intial gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
-function defaultTask(cb) {
-    // place code for your default task here
+const { series } = require('gulp');
+
+
+// TODO: Add bundling feature here
+function build (cb) {
     cb();
-  }
-  
-  exports.default = defaultTask
+}
+
+// TODO: Add Cypress testing here
+function test (cb) {
+    cb();
+}
+
+// TODO: Add server start and firefox open here
+function inspect (cb) {
+    cb();
+}
+
+exports.inspect = inspect;
+exports.default = series(build, test);
